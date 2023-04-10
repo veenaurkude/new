f6d7af777ece9dbbc04c6e5e419371173399ab4c
veenaurkude <veenaurkude10@gmail.com>
2023-04-10 22:56:16 +0530

    React Master-Task2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [msg, setMsg] = useState([]);
+  const mousehover = () => {
+    setMsg([
+      ...msg, 
+      { time: new Date().toLocaleString(), eventname: 'Mouse Hover', tag:"h1" },
+    ]);
+    // console.log(msg);
+  };
+
+  const mouseouts = () => {
+    setMsg([
+      ...msg,
+      { time: new Date().toLocaleString(), eventname: 'Mouse out',tag:"h1" },
+    ]);
+  };
+
+  const mousehoverh2 =() =>{
+    setMsg([
+      ...msg,
+      { time: new Date().toLocaleString(), eventname: 'Mouse Hover', tag:"h2"
+      },
+    ]);
+  }
+
+  const mouseoutsh2 = () => {
+    setMsg([
+      ...msg,
+      { time: new Date().toLocaleString(), eventname: 'Mouse out',tag:"h2" },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 onMouseOver={mousehover} onMouseOut={mouseouts}>
+        I am H1
+      </h1>
+      <h2 onMouseOver={mousehoverh2} onMouseOut={mouseoutsh2}>I am H2</h2>
+      <div> 
+        <table>
+          <thead>
+            <tr>
+              <td>Time</td>
+              <td>Event</td>
+              <td>Tag</td>
+            </tr>
+          </thead>
+          <tbody>
+            {msg.map((el) => (
+              <tr>
+                <td>
+                  {el.time} 
+                </td>
+                <td>
+                  {el.eventname}
+                </td>
+                <td>
+                  {el.tag}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
